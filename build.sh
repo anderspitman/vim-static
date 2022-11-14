b7ca264ca6717e5b957cb26759f0c90326e2e216
Anders Pitman <tapitman11@gmail.com>
2022-11-14 16:58:28 -0700

    Update alpine and vim versions

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-docker run -i --rm -v "$PWD":/out -w /root alpine /bin/sh <<EOF
+docker run -i --rm -v "$PWD":/out -w /root alpine:3.16 /bin/sh <<EOF
 apk add gcc make musl-dev ncurses-static
-wget https://github.com/vim/vim/archive/v8.1.1045.tar.gz
-tar xvfz v8.1.1045.tar.gz
+wget https://github.com/vim/vim/archive/v9.0.0883.tar.gz
+tar xvfz v9.0.0883.tar.gz
 cd vim-*
 LDFLAGS="-static" ./configure --disable-channel --disable-gpm --disable-gtktest --disable-gui --disable-netbeans --disable-nls --disable-selinux --disable-smack --disable-sysmouse --disable-xsmp --enable-multibyte --with-features=huge --without-x --with-tlib=ncursesw
-make
+make -j32
 make install
 mkdir -p /out/vim
 cp -r /usr/local/* /out/vim
